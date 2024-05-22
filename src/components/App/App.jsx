51a585eabc25292eb3891a259c4b064b8c47fbc8
Svitlana Saharieva <149310522+Svitlana3513@users.noreply.github.com>
2024-05-22 20:52:58 +0100

    add

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import css from './App.module.css';
+
+
+
+
+export default function App() {
+    
+
+    return (
+      <div>
+            <h1>Phonebook</h1>
+            
+
+      </div>
+)
+}
+
